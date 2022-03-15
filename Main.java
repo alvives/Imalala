5211e43daa2f684468cbf0fc9e585eb3cbaa0469
XinxiangZ <100697002+XinxiangZ@users.noreply.github.com>
2022-03-15 13:49:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main
 {
     public static void main(String args[])
     {
-        System.out.println("Bienvenido a Imalala SA, la mejor agencia de viaje");
+        System.out.println("Bienvenido a Imalala SA, la mejor agencia de viajes");
     }
 }
 
